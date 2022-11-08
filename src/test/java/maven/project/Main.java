723e7ada133a77ce7add1b72dea79b6d2a9fe9d2
Sunilsyadav16 <ee.sunny11@gmail.com>
2022-11-08 15:13:57 +0530

    Today Change

--- src/test/java/maven/project/Main.java
+++ src/test/java/maven/project/Main.java
@@ -1,9 +1,23 @@
 package maven.project;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Main {
 
-	public static void main(String[] args) {
+	@Test
+	public void startDriver() throws InterruptedException{
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		
+		driver.get("https://ultimateqa.com/complicated-page");
+		
+		Thread.sleep(2000);
+		
+		driver.close();
 
 	}
 
