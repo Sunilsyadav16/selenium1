18cdde630dc8b57a085d9518afe8732eab6bba38
Sunil Yadav <ee.sunny11@gmail.com>
2024-04-08 21:52:31 +0530

    Test a

--- src/test/java/maven/project/Main.java
+++ src/test/java/maven/project/Main.java
@@ -1,9 +1,10 @@
 package maven.project;
 
-import jdk.internal.org.jline.terminal.TerminalBuilder;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -19,23 +20,25 @@ public class Main {
 		
 		driver.get("https://ultimateqa.com/complicated-page");
 		
-		Thread.sleep(2000);
-		
-		driver.close();
 
 	}
 
 	@Test
 	public void pageTitle(){
 
-	//	String pageTitle=driver.getCurrentUrl();
-		//System.out.println(pageTitle);
-
-		driver.findElement(By.xpath("//a[@class=\"et_pb_button et_pb_button_0 et_pb_bg_layout_light\"]")).click();
-		String textValue=driver.findElement(By.id("Skills_Improved")).getText();
-	//	System.out.println(textValue);
+	//driver.findElement(By.xpath("//*[@id=\"main-menu\"]/div/div[2]/div[2]/div/span")).click();
+	driver.findElement(By.xpath("(//ul/li[@id=\"menu-item-218392\"]/a)[1]")).click();
+	;
 
 	}
+
+	@AfterTest
+	public void closeDriver(){
+
+		driver.quit();
+
+	}
+
 
 
 }
